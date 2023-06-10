ad179a83155a40056300c27be7e3a3357c060c16
Rohit <rohitmishra0601@gmail.com>
2023-06-10 22:05:48 +0530

    Query to Find all suppliers who provide products in the ‘Seafood’ category

--- query1.sql
+++ query1.sql
@@ -1,4 +1,4 @@
-/*Write a query to get the total number of orders placed by each customer*/
+/*Query to get the total number of orders placed by each customer*/
 
 
 select customer_id, count(order_id) as total_orders
