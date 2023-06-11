63beebe54ee189f5c6be54e570484e2c817c8c7a
Rohit <rohitmishra.rm2106@gmail.com>
2023-06-11 12:02:30 +0530

    remove query1.sql

--- query1.sql
+++
@@ -1,8 +0,0 @@
-/*Query to get the total number of orders placed by each customer*/
-
-
-select customer_id, count(order_id) as total_orders
-from orders
-group by customer_id
-order by total_orders;
-
