bfc3e34534b92cac921ac25ad274e515570e7c40
Rohit <rohitmishra.rm2106@gmail.com>
2023-06-11 13:52:51 +0530

    Query to Calculate the average product price by category

---
+++ query11.sql
@@ -0,0 +1,7 @@
+/*Calculate the average product price by category*/
+
+SELECT c.category_id, c.category_name,p.product_name, AVG(p.unit_price) AS Average_Price
+FROM products as p
+JOIN categories as c
+ON p.category_id = c.category_id
+group by c.category_name
