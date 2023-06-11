01c4af6121119c4bb56c9871c9ec2c72b5fe3840
Rohit <rohitmishra.rm2106@gmail.com>
2023-06-11 11:02:05 +0530

    Find the total sales (Quantity*Unit_Price) for each category of products

---
+++ query4.sql
@@ -0,0 +1,7 @@
+/* Find the total sales (Quantity*Unit_Price) for each category of products */
+
+SELECT categories.category_id, categories.category_name, SUM(order_details.quantity * order_details.unit_price) AS total_sales
+FROM categories
+JOIN products ON categories.category_id= products.category_id
+JOIN order_details ON products.	product_id = order_details.	product_id
+GROUP BY categories.category_id, categories.category_name;
