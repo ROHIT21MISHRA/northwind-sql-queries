39a5982a13ae1c6409cc01c58f59d18970ae9fed
Rohit <rohitmishra.rm2106@gmail.com>
2023-06-12 15:19:02 +0530

    Query to Find the suppliers who supply the top 5 most sold products

---
+++ query33.sql
@@ -0,0 +1,9 @@
+/*Query to Find the suppliers who supply the top 5 most sold products*/
+
+SELECT s.supplier_id, s.company_name, p.product_id, p.product_name, SUM(od.quantity) AS Total_Quantity
+FROM suppliers s
+JOIN products p ON s.supplier_id = p.supplier_id
+JOIN order_details od ON p.product_id = od.product_id
+GROUP BY s.supplier_id, s.company_name, p.product_id, p.product_name
+ORDER BY Total_Quantity DESC
+LIMIT 5;
