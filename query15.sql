0968337d3d5ba0876dbdb1ed754af221bc1b5325
Rohit <rohitmishra.rm2106@gmail.com>
2023-06-12 12:15:10 +0530

    Query to Find the total revenue for the year 2016

--- query15.sql
+++ query15.sql
@@ -1,7 +1,7 @@
 /* Query to Find the total revenue for the year 2016 */
 
-SELECT SUM(od.quantity * p.unitprice) AS totalrevenue
+SELECT SUM(od.quantity * p.unit_price) AS totalrevenue
 FROM orders o
-JOIN orderdetails od ON o.orderid = od.orderid
-JOIN products p ON od.productid = p.productid
-WHERE YEAR(o.orderdate) = 2016;
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+WHERE YEAR(o.order_date) = 2016;
